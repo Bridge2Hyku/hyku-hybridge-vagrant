7a275e764528cdc7c65f3d9250393b994e330062
Sean Watkins <slwatkins@uh.edu>
2018-10-26 12:00:18 -0500

    Update ImageMagick script

--- install_scripts/imagemagick.sh
+++ install_scripts/imagemagick.sh
@@ -4,23 +4,22 @@
 
 echo "Installing ImageMagick"
 
-IMMVERSION=6.9.10-12
 SHARED_DIR=$1
 
 if [ -f "$SHARED_DIR/install_scripts/config" ]; then
   . $SHARED_DIR/install_scripts/config
 fi
 
-if [ ! -f "$DOWNLOAD_DIR/ImageMagick-$IMMVERSION.tar.gz" ]; then
+if [ ! -f "$DOWNLOAD_DIR/ImageMagick.tar.gz" ]; then
   echo -n "Downloading ImageMagic..."
-  wget -q "https://www.imagemagick.org/download/ImageMagick-$IMMVERSION.tar.gz" -O "$DOWNLOAD_DIR/ImageMagick-$IMMVERSION.tar.gz"
+  wget -q "https://www.imagemagick.org/download/ImageMagick.tar.gz" -O "$DOWNLOAD_DIR/ImageMagick.tar.gz"
   echo " done"
 fi
 
-cp $DOWNLOAD_DIR/ImageMagick-$IMMVERSION.tar.gz /tmp
+cp $DOWNLOAD_DIR/ImageMagick.tar.gz /tmp
 cd /tmp
-tar -xzf ImageMagick-$IMMVERSION.tar.gz
-cd ImageMagick-$IMMVERSION
+tar -xzf ImageMagick.tar.gz
+cd ImageMagick-*
 ./configure --with-openjp2=yes
 make && make install
 ldconfig /usr/local/lib
